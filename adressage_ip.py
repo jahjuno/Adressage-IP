395e61ddcb59cb50724d59a02f0eb2f153004583
jahjuno <aina.juno1806@gmail.com>
2023-06-22 11:26:20 +0300

    Vérification IP et récupération + vérification Mask

--- adressage_ip.py
+++ adressage_ip.py
@@ -19,7 +19,21 @@ def verify_ip_ad(ad_ip):
             if conv_list[i] > 255 :
                 print("error")
                 ad_ip=input("Invalide IP\n Adresse Réseau:")
-           
+        print("Adresse réseau : ", ad_ip)  
 
    
 verify_ip_ad(net_ip_ad)
+
+
+net_mask = input("Masque : ")
+while not re.match(r"^2{1}55\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$", net_mask):
+    net_mask = input("Invalide Masque\n Masque : ")
+
+subnet_number = int(input("Nombre de sous-réseau :"))
+i=0
+while i < subnet_number:
+    print("Sous-réseau n°",i+1)
+    npc_number = input("Nombre de machine:")
+    i = i + 1
+#End__retrieve values IP and Mask
+
