9b06675b9a33b9c856c36e9ac83b6658b4faf37b
jahjuno <aina.juno1806@gmail.com>
2023-07-04 14:02:58 +0300

    retriev Ip&Mask oK and convertion mask to CIDR notation

--- adressage_ip.py
+++ adressage_ip.py
@@ -6,34 +6,43 @@ print('''
             #########################################################################
 ''')
 
-#retrieve values IP, Mask and number of subnetwork
-net_ip_ad=input("Adresse Réseau :")
+#retrieve values IP
 
-def verify_ip_ad(ad_ip):
-    while not re.match("^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$", ad_ip):
-        ad_ip=input("Invalide IP\n Adresse Réseau:")
-        list_v = ad_ip.split(".")
-        conv_list = list(map(int, list_v))
-        print(conv_list)
-        for i in range(len(conv_list)):
-            if conv_list[i] > 255 :
-                print("error")
-                ad_ip=input("Invalide IP\n Adresse Réseau:")
-        print("Adresse réseau : ", ad_ip)  
+net_ip_ad, i = '', 0
+while not re.match(r'^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$', net_ip_ad):
+    net_ip_ad = input("Entrez l'addresse réseau >>>  "
+        if i == 0 else "Invalide IP \nEntrez l'addresse réseau >>> ")
+    i += 1
 
-   
-verify_ip_ad(net_ip_ad)
+#retrieve Network Mask 
+subnet_mask,y= '', 0
+while not re.match(r'2{1}55.\d{1,3}.\d{1,3}.\d{1,3}$', subnet_mask):
+    subnet_mask = input("Entrez le masque >> "
+        if y == 0 else "Invalide Masque \n Entrez le masque >>")
+    y += 1
+    # Convertion mask notation to CIDR notation
+    mask_list = ["255.0.0.0", "255.128.0.0", "255.192.0.0", "255.224.0.0", "255.240.0.0", "255.248.0.0", "255.252.0.0", "255.254.0.0",
+                 "255.255.0.0", "255.255.128.0", "255.255.192.0", "255.255.224.0", "255.255.248.0", "255.255.252.0", "255.255.254.0",
+                 "255.255.255.0", "255.255.255.128", "255.255.255.192", "255.255.255.224", "255.255.255.248", "255.255.255.252", "255.255.255.254"
+                 ]
+    cidr_mask__list = ["/8", "/9", "/10", "/11", "/12", "/13", "/14", "/15", "/16", "/17", "/18", "/19", "/20", "/21", "/22",
+                       "/23", "/24", "/25", "/26", "/27", "/28", "/29", "/30"
+                       ]
+    if subnet_mask in mask_list:
+        index = mask_list.index(subnet_mask)
+        element = cidr_mask__list[index]
+
+        print(subnet_mask, element)
 
 
-net_mask = input("Masque : ")
-while not re.match(r"^2{1}55\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$", net_mask):
-    net_mask = input("Invalide Masque\n Masque : ")
+#retrieve Number of subnet 
+nb_subnet = ''
+i = 0
+while not re.match(r'\d+$', nb_subnet):
+    nb_subnet = input("\nEntrez le nombre de sous réseau >>> "
+        if i == 0 else "Ecrivez un nombre !!! \nEntrer le nombre de sous réseau >>> ")
+    i += 1
 
-subnet_number = int(input("Nombre de sous-réseau :"))
-i=0
-while i < subnet_number:
-    print("Sous-réseau n°",i+1)
-    npc_number = input("Nombre de machine:")
-    i = i + 1
-#End__retrieve values IP and Mask
+
+
 
